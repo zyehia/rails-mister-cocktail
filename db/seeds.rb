60ad7fd882a3faea03f67fc15bfca2aa096f4587
Ziyad Yehia <ziyadyehia93@gmail.com>
2020-11-12 06:16:06 +0000

    Seeded ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "panthers blood")
+Ingredient.create(name: "unicorn hoof")
+Ingredient.create(name: "orc's wart")
+Ingredient.create(name: "wolverine's claw")
+Ingredient.create(name: "lock of centaurs hair")
+Ingredient.create(name: "strawberry")
+
